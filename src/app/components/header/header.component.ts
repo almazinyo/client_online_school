c4b7a5d8182db9db0a72512094761e81184efa9c
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-10-14 22:06:01 +0300

    Правка верхнего меню

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,28 @@
 import {Component} from '@angular/core';
+import {SectionService} from '../section/section.service';
+import {MenuService} from '../menu/menu.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  constructor() {
+  menu: InterFaceMenu[] = [];
+
+  constructor(private sectionService: SectionService,
+              private router: Router,
+              private menuService: MenuService) {
+    this.menuService.getMenu().then((data: InterFaceMenu[]) => {
+        this.menu = data;
+      },
+      (error) => {
+        console.log('Ошибка при получении меню: ', error);
+      });
+  }
+
+  // переход на ссылку
+  getSection(slug) {
+    this.router.navigate(['/section/' + slug]);
   }
 }
