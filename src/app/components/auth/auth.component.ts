701f5d8ff4bfecf003edc48d86a9242ba729ae1a
samovsisyan <samovsisyan9@gmail.com>
2019-10-25 02:35:46 +0400

    Reviews the main page

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -7,11 +7,17 @@ import {MenuService} from '../menu/menu.service';
   templateUrl: './auth.component.html',
 })
 export class AuthComponent {
+user: any;
+output: any;
+
+
+
   data: InterFaceMain[] = [];
   menu: InterFaceMenu[] = [];
 
   constructor(private authService: AuthService,
               private menuService: MenuService) {
+
 
     this.authService.getData().then((data: { mainSection: InterFaceMain[] }) => {
         this.data = data.mainSection;
@@ -26,5 +32,56 @@ export class AuthComponent {
       (error) => {
         console.log('Ошибка при получении меню: ', error);
       });
+
+
+    this.user = [
+      {
+        output: "Отзывы",
+        aboutCourses: "о курсах",
+        allReviews: "Все отзывы",
+         },
+    ];
+
+    this.output = [
+      {
+        img: "assets/imgs/users/1.svg",
+        name: "Максим Мироненко",
+        thumb: "Курс Математики / Оценка ",
+        rating: "5",
+        text: "Курс отличный, очень полезный. Я считаю, что обязателен для всех, кто хочет развиваться в этой сфере. Сложен для",
+
+      },
+      {
+        img: "assets/imgs/users/2.svg",
+        name: "Александр Баль",
+        thumb: "Курс Математики / Оценка ",
+        rating: "5",
+        text: "Курс отличный - ничего лишнего - методички совпадают с излагаемым материалом - преподаватель старается разжевать\n" +
+          "        на первый взгляд непростые темы, очень все достойно, - наконец то ушли от клик митинга - зум рулит.\n" +
+          "     ",
+
+      },
+      {
+        img: "assets/imgs/users/1.svg",
+        name: "Максим Мироненко",
+        thumb: "Курс Математики / Оценка ",
+        rating: "5",
+        text: "Курс отличный - ничего лишнего - методички совпадают с излагаемым материалом - преподаватель старается разжевать\n" +
+          "        на первый взгляд непростые темы, очень все достойно, - наконец то ушли от клик митинга - зум рулит.\n" +
+          "     ",
+
+      },
+      {
+        img: "assets/imgs/users/2.svg",
+        name: "Александр Баль",
+        thumb: "Курс Математики / Оценка ",
+        rating: "5",
+        text: "Курс отличный - ничего лишнего - методички совпадают с излагаемым материалом - преподаватель старается разжевать\n" +
+          "        на первый взгляд непростые темы, очень все достойно, - наконец то ушли от клик митинга - зум рулит.\n" +
+          "     ",
+
+      }
+    ];
+
   }
 }
