c37ccd7a004798dcba3d9b57a1be8f4af72c2a17
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-11-11 08:21:29 +0300

    Форма авторизации

--- src/app/components/modal_auth/modal_auth.component.ts
+++ src/app/components/modal_auth/modal_auth.component.ts
@@ -1,9 +1,15 @@
 import {Component} from '@angular/core';
+import {GlobalParamsAuth} from '../../storage/global-params-auth';
 
 @Component({
   selector: 'app-modal-auth',
   templateUrl: './modal_auth.component.html',
 })
 export class ModalAuthComponent {
+  constructor(public globalParamsAuth: GlobalParamsAuth) {
+  }
 
+  close() {
+    this.globalParamsAuth.showModalAuth = false;
+  }
 }
