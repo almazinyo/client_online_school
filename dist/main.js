78c1a5eab86d6755fded256885c35df83bedab1b
a.papikyan <a.papikyan@corp.mail.ru>
2019-11-24 18:25:46 +0400

    fix

--- dist/main.js
+++ dist/main.js
@@ -3803,7 +3803,7 @@ var HttpService = /** @class */ (function () {
         };
         var headers = new _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpHeaders"]();
         if (api === 'api/main/init/') {
-            return this.http.post('http://online-school/' + api, request, { headers: headers })
+            return this.http.post('http://api.examator.ru/' + api, request, { headers: headers })
                 // return this.http.post('http://u68857.netangels.ru/' + api, request, {headers: headers})
                 // return this.http.post('http://artdekor-kzn.ru/' + api, request, {headers: headers})
                 .pipe(Object(rxjs_internal_operators__WEBPACK_IMPORTED_MODULE_3__["catchError"])(HttpService_1.handlerError));
