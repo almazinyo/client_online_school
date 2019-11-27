7d697e05e303d66e5bde6064c858843a2aeaef21
a.papikyan <a.papikyan@corp.mail.ru>
2019-11-27 13:38:25 +0400

    HotFix: change api url

--- src/app/utils/http/http.service.ts
+++ src/app/utils/http/http.service.ts
@@ -83,14 +83,14 @@ export class HttpService {
 
     if (post) {
       // return this.http.get('http://localhost:8005/' + api + '?' + data, {headers: headers})
-      return this.http.post('http://online-school/' + api, request, {headers: headers})
+      return this.http.post('http://api.examator.ru/' + api, request, {headers: headers})
         .pipe(
           catchError(HttpService.handlerError)
         );
     }
 
     // return this.http.get('http://localhost:8005/' + api, {headers: headers}).pipe(
-      return this.http.get('http://online-school/' + api, {headers: headers}).pipe(
+      return this.http.get('http://api.examator.ru/' + api, {headers: headers}).pipe(
       catchError(HttpService.handlerError)
     );
   }
