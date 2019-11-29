0d6886566a5cbbb863b9c2ab0a0dee0c773b0819
a.papikyan <a.papikyan@corp.mail.ru>
2019-11-29 08:12:14 +0400

    fix

--- src/app/utils/http/http.service.ts
+++ src/app/utils/http/http.service.ts
@@ -85,7 +85,7 @@ export class HttpService {
       headers.append('Content-Type', 'application/json');
 
       // return this.http.post('http://localhost:8005/' + api + '?' + request, {headers: headers})
-      return this.http.post('http://api.examator.ru/' + api, {'test': 5}, {headers: headers})
+      return this.http.post('http://api.examator.ru/' + api, request, {headers: headers})
         .pipe(
           catchError(HttpService.handlerError)
         );
