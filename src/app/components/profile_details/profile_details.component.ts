22bc35f570b92265ceb92914d0af9f7e652cb44d
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-12-06 08:36:03 +0300

    Валидация профиля

--- src/app/components/profile_details/profile_details.component.ts
+++ src/app/components/profile_details/profile_details.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {ProfileDetailsService} from './profile_details.service';
+import {GlobalParamsMessage} from '../message_alert/global-params-message';
 
 @Component({
   selector: 'app-profile-details',
@@ -17,7 +18,8 @@ export class ProfileDetailsComponent {
     city: ''
   };
 
-  constructor(private profileDetailsService: ProfileDetailsService) {
+  constructor(private profileDetailsService: ProfileDetailsService,
+              private globalParamsMessage: GlobalParamsMessage) {
     this.profileDetailsService.getProfileDetails().then((data: InterFaceProfileDetails) => {
         this.profile = data;
       },
@@ -28,6 +30,21 @@ export class ProfileDetailsComponent {
 
   // сохранение информации
   save() {
+    if (this.profile.first_name === '') {
+      this.globalParamsMessage.data = {title: 'Ошибка', body: 'Необходимо указать имя', type: 'error'};
+      return false;
+    }
+
+    if (this.profile.last_name === '') {
+      this.globalParamsMessage.data = {title: 'Ошибка', body: 'Необходимо указать фамилию', type: 'error'};
+      return false;
+    }
+
+    if (this.profile.phone.length !== 11) {
+      this.globalParamsMessage.data = {title: 'Ошибка', body: 'Необходимо указать телефон', type: 'error'};
+      return false;
+    }
+
     this.profile.phone = this.profile.phone.replace(/[\),\(,\-,+,\s]/g, '');
     this.profileDetailsService.save(this.profile).then(() => {
         console.log('Данные успешно сохранены');
