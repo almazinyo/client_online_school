744de590ad72f2a3bafce3e0e537900373884441
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-12-12 13:30:16 +0300

    Вывод изображения

--- src/app/components/teacher-detalis/teacherDetalis.component.ts
+++ src/app/components/teacher-detalis/teacherDetalis.component.ts
@@ -7,6 +7,7 @@ import {TeacherDetalisService} from './teacher-detalis.service';
   templateUrl: './teacherDetalis.component.html',
 })
 export class TeacherDetalisComponent {
+  showCheckImg = false;
   teacherData: InterFaceTeacherDetalis[] = [];
   constructor(private teacherDetalisService: TeacherDetalisService,
               private activatedRoute: ActivatedRoute) {
