351affcb9e9c230ffa4c3d5838d60850604ecaca
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-12-25 08:15:47 +0300

    Переделать авторизацию

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from './components/auth/auth.service';
 import {CookieService} from 'ngx-cookie-service';
 import {SessionStorageService} from './storage/session-storage.service';
@@ -9,29 +9,31 @@ import {Location} from '@angular/common';
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  checkAuth = false;
+  urlAuth = ['/profile-details', '/profile-promotion', '/profile-buy', '/profile-points', '/profile-promotional-code'];
 
   constructor(private authService: AuthService,
               private router: Router,
               private location: Location,
-              private sessionStorageService: SessionStorageService,
-              private cookieService: CookieService) {
+              private sessionStorageService: SessionStorageService) {
 
-    const cookie = this.cookieService.get('vk_app_7200615') || '';
-
-    if (cookie !=='') {
-      this.authService.getInit(cookie).then(() => {
-        },
-        (error) => {
-          console.log('Ошибка при получении информации о клиенте: ', error);
-        });
+    if (!this.checkAuth && this.urlAuth.indexOf(this.location.path()) !== -1) {
+      this.router.navigate(['/']);
     }
+  }
+
+  ngOnInit() {
+    this.authService.getInit().then(() => {
+      },
+      (error) => {
+        console.log('Ошибка при получении информации о клиенте: ', error);
+      });
 
     this.sessionStorageService.authenticated.subscribe(item => {
-      if (!item && ['/profile-details', '/profile-promotion', '/profile-buy', '/profile-points', '/profile-promotional-code'].indexOf(this.location.path())) {
-        this.router.navigate(['/']);
-      }
+      this.checkAuth = item;
     });
+
   }
 }
 
--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -2,12 +2,14 @@ import {Injectable} from '@angular/core';
 import {HttpService} from '../../utils/http/http.service';
 import {SessionStorageService} from '../../storage/session-storage.service';
 import {GlobalParamsUser} from '../../storage/global-params-user';
+import {CookieService} from 'ngx-cookie-service';
 
 @Injectable()
 export class AuthService {
 
   constructor(private httpService: HttpService,
               private sessionStorage: SessionStorageService,
+              private cookieService: CookieService,
               private globalParamsUser: GlobalParamsUser) {
   }
 
@@ -44,24 +46,35 @@ export class AuthService {
   }
 
   // получение списка активных полей
-  public getInit(data) {
+  public getInit() {
     return new Promise((resolve, reject) => {
-      this.httpService.prepareQuery('api/main/init/', {data: data}, true)
-        .then((result: { token: string, username: string }) => {
-            this.sessionStorage.authenticated.emit(true);
 
-            if (typeof result.token !== 'undefined') {
-              this.globalParamsUser.fio = result.username;
-              this.sessionStorage.tokenId = result.token;
+      const cookie = this.cookieService.get('vk_app_7200615') || '';
+
+      if (cookie !== '') {
+        this.httpService.prepareQuery('api/main/init/', {data: cookie}, true)
+          .then((result: { token: string, username: string }) => {
+
+
+              if (typeof result.token !== 'undefined') {
+                this.sessionStorage.authenticated.emit(true);
+                this.globalParamsUser.fio = result.username;
+                this.sessionStorage.tokenId = result.token;
+              } else {
+                this.sessionStorage.authenticated.emit(false);
+              }
+
+              resolve(result);
+            },
+            (error) => {
+              console.log('Ошибка при получении информации о клиенте 2', error);
+              reject();
             }
-
-            resolve(result);
-          },
-          (error) => {
-            console.log('Ошибка при получении информации о клиенте 2', error);
-            reject();
-          }
-        );
+          );
+      } else {
+        this.sessionStorage.authenticated.emit(false);
+        reject();
+      }
     });
   }
 }
